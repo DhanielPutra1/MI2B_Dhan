0dd31b4f6c323b601e7691c21f8994e99d6bb531
DhanielPutra1 <118923684+DhanielPutra1@users.noreply.github.com>
2023-03-14 14:30:51 +0700

    Pert 2

---
+++ src/main/java/p2/KelasKu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2;
+
+/**
+ *
+ * @author User
+ */
+public class KelasKu {
+    public static void main(String[] args){
+    Person objKu = new Person();
+    objKu.setNama("Dhaniel");
+    System.out.println("Nama : "+objKu.getNama());
+    }
+}
---
+++ src/main/java/p2/Person.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2;
+
+/**
+ *
+ * @author User
+ */
+public class Person {
+    String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
